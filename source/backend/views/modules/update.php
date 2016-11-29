24724e0b58e802f3c9f3ea337d26ab9007a94155
toanhv <toanhv.itvn@gmail.com>
2016-11-29 16:42:05 +0700

    up

--- source/backend/views/modules/update.php
+++ source/backend/views/modules/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('backend', 'Update {modelClass}: ', [
     'modelClass' => 'Modules',
-]) . ' ' . $model->id;
+]) . ' ' . $model->getModuleId() . ' - ' . \yii\helpers\Html::encode($model->name);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('backend', 'Modules'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('backend', 'Update');
