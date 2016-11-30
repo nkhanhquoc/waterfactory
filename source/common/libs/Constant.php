e1c65232f0c9c2ef6b20996a80bd30434816bbbe
toanhv <toanhv.itvn@gmail.com>
2016-11-30 21:43:38 +0700

    up

--- source/common/libs/Constant.php
+++ source/common/libs/Constant.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HoangL
- * Date: 12/5/2015
- * Time: 7:03 PM
- */
 
 namespace common\libs;
 
+class Constant {
 
-class Constant
-{
     const ACTIVE = 1;
+
 }
