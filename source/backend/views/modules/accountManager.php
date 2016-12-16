c5f90a6437726827631c66eb36fbafbe7ed00951
toanhv <toanhv.itvn@gmail.com>
2016-12-16 22:54:22 +0700

    up

--- source/backend/views/modules/accountManager.php
+++ source/backend/views/modules/accountManager.php
@@ -8,35 +8,35 @@ $this->params['breadcrumbs'][] = $model->getModuleId() . ' - ' . \yii\helpers\Ht
 
 
 <div class="info-diagram">
-      <div class="check-account">
+    <div class="check-account">
         <form id="manager-form" method="post" action="/index.php/modules/accountmanager?id=<?php echo $model->id ?>">
-          <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
-          <input type="hidden" name="check" value="1">
-          <input type="hidden" id="module-id" value="<?php echo $model->id ?>">
-          <h3 class="title">ID: <?php echo $model->id ?></h3>
-          <div class="row-check-account">
-  		          <a  class="link" onclick="$('#manager-form').submit()">Check <br>Account</a>
-    		      <div class="content">
-              	<div class="text-02">Số tiền còn lại trong tài khoản của bạn là : <strong id="money-info"><?php echo number_format($model->money) ?></strong> đ</div>
-              </div>
-          </div>
-          <div class="row-check-account">
-        		  <a href="javascript:void(0)" onclick="$('#manager-form').submit()" class="link">Check <br>Data</a>
-        		  <div class="content">
-           	 <div class="text-02">Data con lại của bạn là : <strong id="data-info"><?php echo number_format($model->data) ?></strong> KB</div>
+            <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
+            <input type="hidden" name="check" value="1">
+            <input type="hidden" id="module-id" value="<?php echo $model->id ?>">
+            <h3 class="title">ID: <?php echo $model->getModuleId() . ' - ' . \yii\helpers\Html::encode($model->name); ?></h3>
+            <div class="row-check-account">
+                <a  class="link" onclick="$('#manager-form').submit()">Check <br>Account</a>
+                <div class="content">
+                    <div class="text-02">Số tiền còn lại trong tài khoản của bạn là : <strong id="money-info"><?php echo number_format($model->money) ?></strong> đ</div>
+                </div>
             </div>
-          </div>
+            <div class="row-check-account">
+                <a href="javascript:void(0)" onclick="$('#manager-form').submit()" class="link">Check <br>Data</a>
+                <div class="content">
+                    <div class="text-02">Data con lại của bạn là : <strong id="data-info"><?php echo number_format($model->data) ?></strong> KB</div>
+                </div>
+            </div>
         </form>
-          <form action="/index.php/modules/accountmanager?id=<?php echo $model->id ?>" id="pay_card_form" method="post">
+        <form action="/index.php/modules/accountmanager?id=<?php echo $model->id ?>" id="pay_card_form" method="post">
             <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
             <input type="hidden" name="pay" value="1">
             <div class="row-check-account">
-              <button class="link" onclick="return checkCard()">Change <br>Account</button>
-    		      <div class="content">
-                  <div class="text-02">
-                    <input type="text" id="card_info" name="card_info" placeholder="Enter your code" class="text-field">
-                  </div>
-              </div>
+                <button class="link" onclick="return checkCard()">Change <br>Account</button>
+                <div class="content">
+                    <div class="text-02">
+                        <input type="text" id="card_info" name="card_info" placeholder="Enter your code" class="text-field">
+                    </div>
+                </div>
             </div>
         </form>
     </div>
@@ -75,24 +75,24 @@ $this->params['breadcrumbs'][] = $model->getModuleId() . ' - ' . \yii\helpers\Ht
 
     }, 10000);
 
-  function updateModuleInput(){
-    $('#module-id').val($('#module_id').val());
-    loadInfo();
-  }
-
-  function checkCard(){
-    var card = $('#card_info').val();
-    if(card.length < 12|| card.length > 16){
-      alert("Mã thẻ cào chưa chính xác!");
-       $('#card_info').focus();
-       return false;
+    function updateModuleInput() {
+        $('#module-id').val($('#module_id').val());
+        loadInfo();
     }
 
-    if(!$.isNumeric(card)){
-      alert("Mã thẻ cào chưa chính xác!");
-       $('#card_info').focus();
-       return false;
+    function checkCard() {
+        var card = $('#card_info').val();
+        if (card.length < 12 || card.length > 16) {
+            alert("Mã thẻ cào chưa chính xác!");
+            $('#card_info').focus();
+            return false;
+        }
+
+        if (!$.isNumeric(card)) {
+            alert("Mã thẻ cào chưa chính xác!");
+            $('#card_info').focus();
+            return false;
+        }
+        $('#pay_card_form').submit();
     }
-    $('#pay_card_form').submit();
-  }
 </script>
