7c1123f5aa8573ee9d6e7abdd2077600d495d858
toanhv <toanhv.itvn@gmail.com>
2016-12-20 20:44:54 +0700

    up

--- source/backend/views/mode/index.php
+++ source/backend/views/mode/index.php
@@ -2,50 +2,48 @@
 
 use backend\models\ModeSearch;
 use yii\data\ActiveDataProvider;
-use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\web\View;
-use yii\widgets\Pjax;
 
 /* @var $this View */
 /* @var $searchModel ModeSearch */
 /* @var $dataProvider ActiveDataProvider */
 
-$this->title = Yii::t('backend', 'Modes');
-$this->params['breadcrumbs'][] = $this->title;
+$idModule = $module->country->code . $module->privincial->code . $module->distric->code . $module->customer_code;
+$this->title = $idModule . ' - ' . $module->name;
 ?>
 <div class="info-diagram">
-  <form method="post" id="form-choose-mode" action="/index.php/modules/mode?id=<?php echo $module->id ?>">
-    <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
-    <!-- <?= Html::csrfMetaTags() ?> -->
-  <div class="check-account">
-            <h3 class="title">ID: <?php echo $module->id ?></h3>
+    <form method="post" id="form-choose-mode" action="/index.php/modules/mode?id=<?php echo $module->id ?>">
+        <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
+        <!-- <?= Html::csrfMetaTags() ?> -->
+        <div class="check-account">
+            <h3 class="title"><?php echo $this->title; ?></h3>
             <p align="center">Choose your system:</p>
             <div class="row-check-account">
-              <input type="hidden" value="" name="mode_id" id="mode_id">
-              <p align="center">
-                <?php foreach($modes as $mode): ?>
-                  <a href="javascript:void(0)" id="mode_<?php echo $mode->id ?>" onclick="chooseMode('<?php echo $mode->id ?>')" class="btn-check <?php if($module->mode_id == $mode->id) echo 'chosen' ;?>">
-                    <!-- <img src="<?php echo $mode->getUrlImage(300,220) ?>" alt="<?php echo $mode->name ?>"/> -->
-                    <?php echo $mode->getUrlImage(300,220) ?>
-                  </a>
-                <?php endforeach; ?>
-              </p>
-              <p align="center"><a href="javascript:void(0)" onclick="$('#form-choose-mode').submit()" class="btn-link">Next</a></p>
+                <input type="hidden" value="" name="mode_id" id="mode_id">
+                <p align="center">
+                    <?php foreach ($modes as $mode): ?>
+                        <a href="javascript:void(0)" id="mode_<?php echo $mode->id ?>" onclick="chooseMode('<?php echo $mode->id ?>')" class="btn-check <?php if ($module->mode_id == $mode->id) echo 'chosen'; ?>">
+                          <!-- <img src="<?php echo $mode->getUrlImage(300, 220) ?>" alt="<?php echo $mode->name ?>"/> -->
+                            <?php echo $mode->getUrlImage(300, 220) ?>
+                        </a>
+                    <?php endforeach; ?>
+                </p>
+                <p align="center"><a href="javascript:void(0)" onclick="$('#form-choose-mode').submit()" class="btn-link">Next</a></p>
             </div>
-      </div>
-      </form>
+        </div>
+    </form>
 </div>
 
 <script type="text/javascript">
-function chooseMode(id){
-  $('#mode_id').val(id);
-  $('a').removeClass("chosen");
-  $('#mode_'+id).addClass("chosen");
-}
+    function chooseMode(id) {
+        $('#mode_id').val(id);
+        $('a').removeClass("chosen");
+        $('#mode_' + id).addClass("chosen");
+    }
 </script>
 <style>
-  .chosen {
-    background-color: #1caf9a !important;
-  }
+    .chosen {
+        background-color: #1caf9a !important;
+    }
 </style>
