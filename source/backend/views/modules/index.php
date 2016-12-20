d6df7cd533c749354a9184df99a448dd81e40e8b
toanhv <toanhv.itvn@gmail.com>
2016-12-20 20:36:45 +0700

    up

--- source/backend/views/modules/index.php
+++ source/backend/views/modules/index.php
@@ -4,13 +4,18 @@
     if (!empty($data)) {
         foreach ($data as $val) {
             $url = 'javascript:void(0);';
+            $img = $val->getImg();
             if ($val->imsis->status == CONFIRM_STATUS) {
                 $url = '/modules/view?id=' . $val->id;
+                if ($img == MODULE_SETTING) {
+                    \Yii::$app->session->set('module_id', $val->id);
+                    $url = '/mode/index';
+                }
             }
             ?>
             <div class="col-md-4">
                 <a href="<?php echo $url; ?>" title="<?php echo \yii\helpers\Html::encode($val->name); ?>">
-                    <img class="img-responsive" src="<?php echo $val->getImg(); ?>" alt="<?php echo \yii\helpers\Html::encode($val->name); ?>" />
+                    <img class="img-responsive" src="<?php echo $img; ?>" alt="<?php echo \yii\helpers\Html::encode($val->name); ?>" />
                     <p class="">ID: <?php echo \yii\helpers\Html::encode($val->getModuleId()); ?></p>
                     <p class=""><?php echo \yii\helpers\Html::encode($val->name); ?></p>
                 </a>
