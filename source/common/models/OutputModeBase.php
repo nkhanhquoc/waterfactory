7dd8c0bb2f41281d77e635403c70e7ab6ba9c8b4
toanhv <toanhv.itvn@gmail.com>
2016-12-22 00:28:20 +0700

    up

--- source/common/models/OutputModeBase.php
+++ source/common/models/OutputModeBase.php
@@ -4,6 +4,16 @@ namespace common\models;
 
 use Yii;
 
+/**
+ * @property ModulesBase $module
+ */
 class OutputModeBase extends \common\models\db\OutputModeDB {
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getModule() {
+        return $this->hasOne(ModulesBase::className(), ['id' => 'module_id']);
+    }
 
 }
