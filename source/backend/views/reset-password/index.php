2431e70dd9cd7155af5115c8c14a574c86a60a45
toanhv <toanhv.itvn@gmail.com>
2016-12-23 21:47:24 +0700

    up

--- source/backend/views/reset-password/index.php
+++ source/backend/views/reset-password/index.php
@@ -5,14 +5,12 @@ use yii\helpers\Html;
 $this->title = "Reset default password";
 ?>
 <h1><?= Html::encode($this->title) ?></h1>
-<form method="post" action="/index.php/reset-password/index">
+<form method="post" action="/reset-password/index">
     <input type="hidden" name="_csrf" value="<?php Yii::$app->request->csrfToken ?>">
     <div class="params">
         <div class="row100 text-center">
             <h1 class="title"><?php echo $module->getModuleId() . ' - ' . \yii\helpers\Html::encode($module->name); ?></h1>
-            <input type="submit" value="Update" class="btn btn-primary"/>
-            <!-- <input type="button" class="btn btn-primary" value="Send to Module" /> -->
+            <input type="submit" value="Reset" class="btn btn-primary"/>
         </div>
     </div>
-
 </form>
