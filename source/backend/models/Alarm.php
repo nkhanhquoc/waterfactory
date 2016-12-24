fbbefeb3b1d79eb2e5d3da2009e64846d3450690
toanhv <toanhv.itvn@gmail.com>
2016-12-24 17:10:44 +0700

    up

--- source/backend/models/Alarm.php
+++ source/backend/models/Alarm.php
@@ -1,4 +1,4 @@
-\<?php
+<?php
 
 namespace backend\models;
 
@@ -6,15 +6,15 @@ use Yii;
 
 class Alarm extends \common\models\AlarmBase {
 
-  public function getReport($from,$to,$moduleId){
-    $alarms = Alarm::find()->where([
-      '>=','created_at',$from
-    ])->andWhere([
-      '<=','created_at',$to
-    ])->andWhere([
-      'module_id'=>$moduleId
-    ])->all();
-    // var_dump($sensors->createCommand()->getRawSql());die;
-    return $alarms;
-  }
+    public function getReport($from, $to, $moduleId) {
+        $alarms = Alarm::find()->where([
+                    '>=', 'created_at', $from
+                ])->andWhere([
+                    '<=', 'created_at', $to
+                ])->andWhere([
+                    'module_id' => $moduleId
+                ])->all();
+        return $alarms;
+    }
+
 }
