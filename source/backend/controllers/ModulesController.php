903ee0c109e026d826bfd254168f28ca9e8649ea
toanhv <toanhv.itvn@gmail.com>
2017-01-02 00:21:17 +0700

    up

--- source/backend/controllers/ModulesController.php
+++ source/backend/controllers/ModulesController.php
@@ -42,29 +42,8 @@ class ModulesController extends AppController {
     }
 
     public function actionAllView() {
-        return $this->redirect(['view', 'id' => \Yii::$app->session->get('module_id', 0)]);
-    }
-
-    public function actionRefresh() {
-        $moduleId = \Yii::$app->session->get('module_id', 0);
-        $module = Modules::findOne($moduleId);
-        if ($module) {
-            $module->checkSystemStatus();
-            Yii::$app->session->setFlash('success', 'Check SYSTEM STATUS to module success!');
-        }
-
-        return $this->redirect(['view', 'id' => $moduleId]);
-    }
-
-    /**
-     * Displays a single Modules model.
-     * @param string $id
-     * @return mixed
-     */
-    public function actionView($id) {
+        $id = \Yii::$app->session->get('module_id', 0);
         $model = $this->findModel($id);
-
-        \Yii::$app->session->set('module_id', $model->id);
 
         $sensors = $model->sensors;
         $statuses = $model->moduleStatuses;
@@ -102,6 +81,16 @@ class ModulesController extends AppController {
     }
 
     /**
+     * Displays a single Modules model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id) {
+        \Yii::$app->session->set('module_id', $id);
+        return $this->redirect(['all-view']);
+    }
+
+    /**
      * Creates a new Modules model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
@@ -114,6 +103,7 @@ class ModulesController extends AppController {
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             if ($model->toClient()) {
+                //timer counter
                 $timerCounter = new \backend\models\TimerCounter();
                 $timerCounter->module_id = $model->id;
                 $timerCounter->counter = 0;
@@ -122,6 +112,15 @@ class ModulesController extends AppController {
                 $timerCounter->timer_3 = 0;
                 $timerCounter->created_at = new \yii\db\Expression('now()');
                 $timerCounter->save(false);
+                //load mode
+                $loadMode = new \backend\models\OutputMode();
+                $loadMode->module_id = $model->id;
+                $loadMode->save(false);
+                //Param config
+                $paramConfig = new \backend\models\ParamConfig();
+                $paramConfig->module_id = $model->id;
+                $paramConfig->save(false);
+
                 Yii::$app->session->setFlash('success', 'Set ID to module ' . $model->getModuleId() . ' success!');
                 sleep(TIME_OUT_REFRESH);
                 return $this->goHome();
@@ -216,7 +215,7 @@ class ModulesController extends AppController {
     }
 
     public function actionAccountmanager() {
-        $id = \Yii::$app->session->get('module_id');
+        $id = \Yii::$app->session->get('module_id', 0);
         if (!$id) {
             return $this->goHome();
         }
--- source/backend/controllers/OutputModeController.php
+++ source/backend/controllers/OutputModeController.php
@@ -73,106 +73,22 @@ class OutputModeController extends AppController {
     }
 
     /**
-     * Creates a new OutputMode model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate() {
-        $model = new OutputMode();
-        $moduleId = \Yii::$app->session->get('module_id', 0);
-        if (!$moduleId) {
-            return $this->goHome();
-        }
-        $model->module_id = $moduleId;
-        $module = Modules::findOne($moduleId);
-
-        if (Yii::$app->request->isPost) {
-            $values = Yii::$app->request->post();
-            if ($values['OutputMode']['convection_pump']['time'] > 255) {
-                $convectionPumptime = Socket::alldec2bin($values['OutputMode']['convection_pump']['time'], 16);
-            } else {
-                $convectionPumptime = Socket::alldec2bin($values['OutputMode']['convection_pump']['time'], 8) . BACKUP;
-            }
-            $model->convection_pump = $values['OutputMode']['convection_pump']['mode'] . $values['OutputMode']['convection_pump']['pump'] . $convectionPumptime;
-
-            if ($values['OutputMode']['cwsp_pump']['time'] > 255) {
-                $cwspPumptime = Socket::alldec2bin($values['OutputMode']['cwsp_pump']['time'], 16);
-            } else {
-                $cwspPumptime = Socket::alldec2bin($values['OutputMode']['cwsp_pump']['time'], 8) . BACKUP;
-            }
-            $model->cold_water_supply_pump = $values['OutputMode']['cwsp_pump']['mode'] . $values['OutputMode']['cwsp_pump']['pump'] . $cwspPumptime;
-
-            if ($values['OutputMode']['return_pump']['time'] > 255) {
-                $returnPumptime = Socket::alldec2bin($values['OutputMode']['return_pump']['time'], 16);
-            } else {
-                $returnPumptime = Socket::alldec2bin($values['OutputMode']['return_pump']['time'], 8) . BACKUP;
-            }
-            $model->return_pump = $values['OutputMode']['return_pump']['mode'] . $values['OutputMode']['return_pump']['pump'] . $returnPumptime;
-
-            if ($values['OutputMode']['pressure_pump']['time'] > 255) {
-                $pressurePumptime = Socket::alldec2bin($values['OutputMode']['pressure_pump']['time'], 16);
-            } else {
-                $pressurePumptime = Socket::alldec2bin($values['OutputMode']['pressure_pump']['time'], 8) . BACKUP;
-            }
-            $model->incresed_pressure_pump = $values['OutputMode']['pressure_pump']['mode'] . $values['OutputMode']['pressure_pump']['pump'] . $pressurePumptime;
-
-            if ($values['OutputMode']['heat_pump']['time'] > 255) {
-                $heatPumptime = Socket::alldec2bin($values['OutputMode']['heat_pump']['time'], 16);
-            } else {
-                $heatPumptime = Socket::alldec2bin($values['OutputMode']['heat_pump']['time'], 8) . BACKUP;
-            }
-            $model->heat_pump = $values['OutputMode']['heat_pump']['mode'] . $values['OutputMode']['heat_pump']['pump'] . $heatPumptime;
-
-            if ($values['OutputMode']['heater_resis']['time'] > 255) {
-                $heaterResisPumptime = Socket::alldec2bin($values['OutputMode']['heater_resis']['time'], 16);
-            } else {
-                $heaterResisPumptime = Socket::alldec2bin($values['OutputMode']['heater_resis']['time'], 8) . BACKUP;
-            }
-            $model->heater_resister = $values['OutputMode']['heater_resis']['mode'] . $values['OutputMode']['heater_resis']['pump'] . $heaterResisPumptime;
-
-            if ($values['OutputMode']['3way']['time'] > 255) {
-                $twayPumptime = Socket::alldec2bin($values['OutputMode']['3way']['time'], 16);
-            } else {
-                $twayPumptime = Socket::alldec2bin($values['OutputMode']['3way']['time'], 8) . BACKUP;
-            }
-            $model->three_way_valve = $values['OutputMode']['3way']['mode'] . $values['OutputMode']['3way']['pump'] . $twayPumptime;
-
-            if ($values['OutputMode']['blakflow']['time'] > 255) {
-                $blakflowPumptime = Socket::alldec2bin($values['OutputMode']['blakflow']['time'], 16);
-            } else {
-                $blakflowPumptime = Socket::alldec2bin($values['OutputMode']['blakflow']['time'], 8) . BACKUP;
-            }
-            $model->backflow_valve = $values['OutputMode']['blakflow']['mode'] . $values['OutputMode']['blakflow']['pump'] . $blakflowPumptime;
-
-            //thang nay khong co du phong
-            $model->reserved = $values['OutputMode']['reserved']['mode'] . $values['OutputMode']['reserved']['pump'] . Socket::alldec2bin($values['OutputMode']['reserved']['time'], 8);
-
-            if ($model->save(false)) {
-                if ($model->toClient()) {
-                    $model->OperationLog();
-                    $model->configLog();
-                    Yii::$app->session->setFlash('success', 'Set Load Mode to module success!');
-                    // return $this->redirect(['/modules/view', 'id' => $model->module_id]);
-                    return $this->redirect('/param-config/home');
-                }
-            }
-        }
-        return $this->render('create', [
-                    'model' => $model,
-                    'module' => $module
-        ]);
-    }
-
-    /**
      * Updates an existing OutputMode model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param string $id
      * @return mixed
      */
-    public function actionUpdate($id) {
-        $model = $this->findModel($id);
-
-        $module = $model->module;
+    public function actionUpdate() {
+        $moduleId = \Yii::$app->session->get('module_id', 0);
+        if (!$moduleId) {
+            return $this->goHome();
+        }
+        $module = \backend\models\Modules::findOne($moduleId);
+        if ($module && $module->outputModes) {
+            $model = $this->findModel($module->outputModes->id);
+        } else {
+            return $this->goHome();
+        }
         if ($module->mode_id && $_GET['reload'] == 'true') {
             $module->checkOutputMode();
             sleep(TIME_OUT_REFRESH);
